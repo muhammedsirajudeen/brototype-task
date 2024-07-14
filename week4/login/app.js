2c02f8aabb3b1490762ceb583bfce668171a069b
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-07-14 17:28:27 +0530

    finished first structure

--- week4/login/app.js
+++ week4/login/app.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const path=require("path")
+const bodyParser = require('body-parser');
+
 const {engine} = require('express-handlebars');
 
 //user defined
@@ -10,9 +12,12 @@ const app=express()
 
 const PORT=3000
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use('/',authRouter)
 //set formdata handler logic here
+
 
 app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'layout', layoutsDir: path.join(__dirname, 'views', 'layouts'), partialsDir: path.join(__dirname, 'views', 'partials') }));
 app.set('views', path.join(__dirname,'views'))
--- week4/login/route/authentication.js
+++ week4/login/route/authentication.js
@@ -1,7 +1,27 @@
 const express=require("express")
+const placeListing=require('../placeLister')
+
 const router=express.Router()
 
-const placeListing=require('../placeLister')
+const user={
+    username:'sirajudeen',
+    password:'sirajudeen'
+}
+
+router.post('/auth',(req,res)=>{
+    try{
+        let {username,password}=req.body
+        if(username === user.username && password === user.password ){
+            res.json({message:"success"})
+        }else{
+            res.json({message:"invalid credentials"})
+        }
+    
+    }catch(error){
+        console.log(error)
+        res.json({message:"failure"})
+    }
+})
 
 //if session redirect to home
 router.get('/',(req,res)=>{
@@ -17,8 +37,6 @@ router.get('/home',(req,res)=>{
     )
 })
 
-router.post('/auth',(req,res)=>{
-    //get post data here
-})
+
 
 module.exports=router
