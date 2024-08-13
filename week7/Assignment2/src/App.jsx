aefa835ca7aaecb4b2f7ac9b075a7a16c0b14b22
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-08-13 17:28:30 +0530

    completed assigment 2 and 3

--- week7/Assignment2/src/App.jsx
+++ week7/Assignment2/src/App.jsx
@@ -4,17 +4,28 @@ import Card from './Card'
 function App() {
   const [count,setCount]=useState(0)
   const [users,setUsers]=useState({})
+  const [loading,setLoading]=useState(true)
+  const [error,setError]=useState(false)
   useEffect(()=>{
+    console.log("clicked")
     async function userFetcher(){
-      let data=await (await fetch("https://jsonplaceholder.typicode.com/users/1")).json()
-      console.log(data)
-      setUsers(data)
+      try{
+        let data=await (await fetch("https://jsonplaceholder.typicode.com/users/1")).json()
+        console.log(data)
+        setUsers(data)
+        setLoading(false)
+  
+      }catch(error){
+        console.log(error)
+        setLoading(true)
+        setError(true)
+      }
     }
     userFetcher()
   },[])
   return (
     <div className="main-container">
-      <Card users={users}/>
+      <Card users={users} loading={loading} error={error} />
       <h1>COUNT : {count}</h1>
       <button onClick={()=>setCount((prev)=>prev+1)}>+</button>
       <button onClick={()=>setCount((prev)=>prev-1)}>-</button>
--- week7/Assignment2/src/Card.jsx
+++ week7/Assignment2/src/Card.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
 import "./App.css";
 
-export default function Card({ users }) {
+export default function Card({ users,loading,error }) {
   return (
-    <div key={users.id} className="sub-container">
-      <p className="text-white">{users.name}</p>
-      <p>{users.email}</p>
-    </div>
+    <>
+    {error && <h1>ERROR ..</h1>}
+      {loading ? 
+        <h1>Loading ...</h1>
+        :
+        <div key={users.id} className="sub-container">
+          <p className="text-white">{users.name}</p>
+          <p>{users.email}</p>
+        </div>
+      }
+    </>
   );
 }
