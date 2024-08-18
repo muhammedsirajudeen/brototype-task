9d46587e80bea2140724f48bd59f974a3db2638c
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2024-08-18 21:00:11 +0530

    Completed First List of Todos

--- week8/olx-week8/src/components/Recommendations.tsx
+++ week8/olx-week8/src/components/Recommendations.tsx
@@ -1,9 +1,19 @@
-import { ReactElement } from "react";
+import { DocumentData } from 'firebase/firestore'
+import { ReactElement } from 'react'
 
-export default function Recommendations():ReactElement{
-    return(
-        <div className="flex ml-20 items-start justify-center flex-col">
-            <h1 className="font-normal mt-5 text-lg" >Fresh recommendations</h1>
-        </div>
-    )
+export default function Recommendations({
+  products,
+}: {
+  products: Array<DocumentData>
+}): ReactElement {
+  return (
+    <div className="flex ml-20 items-start justify-center flex-col">
+      <h1 className="font-normal mt-5 text-lg">Fresh recommendations</h1>
+      {!products && <h1>hey</h1>}
+      {products.map((product: DocumentData) => {
+        console.log(product.ProductName)
+        return <div key={product.id}>{product.ProductName}</div>
+      })}
+    </div>
+  )
 }
--- week8/olx-week8/src/pages/Home.tsx
+++ week8/olx-week8/src/pages/Home.tsx
@@ -1,16 +1,59 @@
-import { ReactElement, useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
+
+import { DocumentData, getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import app from '../firebaseHelper/firebaseHelper';
+
 import TopBar from '../components/TopBar'
 import CategoryBox from '../components/CategoryBox';
 import Recommendations from '../components/Recommendations';
+
+
 export default function Home(): ReactElement {
     const [dialog,setDialog]=useState<boolean>(false);
-    
+    // the data has differing fields so
+    const [products,setProducts]=useState<Array<DocumentData>>([])
+    const [loading,setLoading]=useState(true)
+    const numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+
+    useEffect(()=>{
+        async function getData(){
+            const db=getFirestore(app)
+            const querySnapshot = await getDocs(collection(db, "Products"));
+            const products:Array<DocumentData>=[]
+            querySnapshot.forEach((doc) => {
+                const data=doc.data()
+                data.id=doc.id
+                console.log(data.Location)
+                products.push(data)
+            });
+            setProducts(products)
+            setLoading(false)
+        }
+        getData()
+    },[])
 
     return (
         <>
             <TopBar setDialog={setDialog} />
             {dialog && <CategoryBox/> }
-            <Recommendations/>
+            <Recommendations products={products} />
+            {loading && 
+                <div className='flex items-start justify-start  w-full h-full '>
+                    {
+                        numbers.map(()=>{
+                            return(
+                                <div className='flex ml-20 items-start justify-start flex-col'>
+                                    <div className='skeleton-loader h-40 w-52'></div>
+                                    <div className='skeleton-loader mt-5 h-10 w-52'></div>
+                                </div>
+        
+                            )
+                        })
+                    }
+                </div>
+            }
+
         </>
     )
 }
